6be2a30b206cf7d00bfea556ca7a67a64994b886
Ubuntu <ubuntu@ip-172-31-92-220.ec2.internal>
2023-01-03 19:39:43 +0000

    changing the package name

--- components/setup.py
+++ components/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 
 setup(
-    name="wandb_utils",
+    name="wandbutils",
     version=0.1,
     description="Utilities for interacting with Weights and Biases and mlflow",
     zip_safe=False,  # avoid eggs, which make the handling of package data cumbersome
-    packages=["wandb_utils"],
+    packages=["wandbutils"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
