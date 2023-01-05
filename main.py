bde6ff3c0d53fce91b7a8c1d60636a75a5a56160
Ubuntu <ubuntu@ip-172-31-92-220.ec2.internal>
2023-01-05 16:01:52 +0000

    updated the basic_cleaning_step

--- main.py
+++ main.py
@@ -47,14 +47,22 @@ def go(config: DictConfig):
                     "artifact_type": "raw_data",
                     "artifact_description": "Raw file as downloaded"
                 },
-                env_manager="virtualenv"
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                "main",
+                parameters={
+                    "tmp_directory": tmp_dir,
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_sample",
+                    "output_description": "Data with outliers and null values removed",
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_check" in active_steps:
             ##################
